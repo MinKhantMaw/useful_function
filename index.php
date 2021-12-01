87fddc48fb9a5204a81bb3492be927177b901ed4
Min Khant Maw <mr.minkhantmaw@gmail.com>
2021-12-01 12:19:41 +0630

    part2&3

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
 <?php
-
+    // define use
     $name="Min Khant Maw";
-    define('Name','Min Khant Maw');
+    define('NAME','Min Khant ');
     echo ($name);
+
+    // header use
+    // header('Location:index.php');
+    // ecryption
+    // echo md5($name);
+    echo "<pre>";
+    date_default_timezone_set("Asia/Yangon");
+    echo date("d-m-Y h:i");
+echo "<pre>";
+
+    $today =date_create(date("d-m-Y"));
+    date_add($today,date_interval_create_from_date_string('3 days'));
+    echo date_format($today,"d-m-Y");
+    echo "</pre>";
+    // include && require
+    include('test.php');
+    echo "<br> this is test include text";
